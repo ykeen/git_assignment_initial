c578afe344b426480f139f564f898ae87081ce90
markihab <36458936+markihab@users.noreply.github.com>
2020-02-28 22:44:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
+			//new SimpleSubscriber(),
+			//new ReallySimpleSubscriber(),
+			new CircleArea(),
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
