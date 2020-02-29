0ae95205e07f9fd2d77482766da425ff6e7d6a3a
fatmaashraframadan <36447147+fatmaashraframadan@users.noreply.github.com>
2020-02-29 23:31:30 +0200

    20170232 - SphereCircumference is Done.

--- src/git_Assignment/Main.java
+++ src/git_Assignment/Main.java
@@ -8,6 +8,8 @@ public class Main {
             new ReallySimpleSubscriber(),
             new sphereArea(),
             new CircleVol(),
+            new SphereVolum(),
+            new SphereCircumference()
     };
     public static void main(String[] args) {
         System.out.println("Enter Your Radius here :");
