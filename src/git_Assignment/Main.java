4c1a9fc4f80ea7ed69c12fe8f24380d0a176e664
mahmoud45 <magdymahmoud913@gmail.com>
2020-03-01 00:16:38 +0200

    Adding class LucasSeries
    Compute the lucas number for a given integer.

--- src/git_Assignment/Main.java
+++ src/git_Assignment/Main.java
@@ -8,6 +8,7 @@ public class Main {
             new ReallySimpleSubscriber(),
             new sphereArea(),
             new CircleVol(),
+            new LucasSeries(),
     };
     public static void main(String[] args) {
         System.out.println("Enter Your Radius here :");
